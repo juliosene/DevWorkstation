ea8bd5cfac97ab346625546cc29afc0eef2ff9a3
Julio Sene França <julio.sene@gmail.com>
2019-05-27 18:24:15 -0300

    Create a Ubuntu Mobile Dev Workstation

---
+++ ubuntu-gcp-boot.sh
@@ -0,0 +1,41 @@
+# Update & Upgrade
+apt-get update
+apt-get upgrade
+#
+# Google SDK
+#
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+# Install Git
+apt-get install git
+
+# Install NPM
+apt install npm
+
+# Install Cordova
+npm install -g cordova
+
+# Install Phonegap
+npm install -g phonegap@latest
+
+# Install Remote Desktop
+sudo apt install xrdp
+sudo systemctl enable xrdp
+
+#  Install Visual Studio
+sudo snap install code --classic
+
+# Init
+gcloud init
+
+
